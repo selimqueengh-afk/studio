857c15613dc21f88142f42fa29027ce8bd6b87a6
Hshsj <hohoberegoi@gmail.com>
2025-07-24 18:19:35 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// IMPORTANT: Replace with your own Firebase project configuration.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_SENDER_ID",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "YOUR_APP_ID",
+  projectId: "sohbet-odas-zfrqc",
+  appId: "1:966575380934:web:6a7e53fe6336859b2d7ac7",
+  storageBucket: "sohbet-odas-zfrqc.firebasestorage.app",
+  apiKey: "AIzaSyBf_fp93ufZ9Jwd9Tunwzr1HZy1JGG_D64",
+  authDomain: "sohbet-odas-zfrqc.firebaseapp.com",
+  messagingSenderId: "966575380934"
 };
 
 // Initialize Firebase
