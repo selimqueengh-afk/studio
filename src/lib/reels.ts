e533cbf58b1c65b91eda48917d0c1d1a299fe706
Hshsj <hohoberegoi@gmail.com>
2025-07-25 22:38:26 +0000

    URL yanliş ozaman

--- src/lib/reels.ts
+++ src/lib/reels.ts
@@ -8,34 +8,36 @@ export interface StaticReel {
 
 // Sourced from Pexels, which provides free-to-use videos.
 // Updated to be more humorous and gaming-oriented per user request.
+// ---
+// UPDATED: Replaced potentially expired Pexels URLs with stable, standard test video URLs to fix loading issues.
 export const staticReels: StaticReel[] = [
     {
-        id: 'pexels-1',
-        videoUrl: 'https://videos.pexels.com/video-files/7578544/7578544-hd.mp4',
+        id: 'sample-1',
+        videoUrl: 'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4',
         author: '@oyun_canavari',
         description: 'Roblox parkur denemem (sonu komik bitti)'
     },
     {
-        id: 'pexels-2',
-        videoUrl: 'https://videos.pexels.com/video-files/8024935/8024935-hd.mp4',
+        id: 'sample-2',
+        videoUrl: 'https://test-videos.co.uk/vids/elephantsdream/mp4/h264/360/Elephants_Dream_360_10s_1MB.mp4',
         author: '@dans_kraliçesi',
         description: 'Bu yeni dans akımını deneyen var mı? #challenge'
     },
     {
-        id: 'pexels-3',
-        videoUrl: 'https://videos.pexels.com/video-files/5960076/5960076-hd.mp4',
+        id: 'sample-3',
+        videoUrl: 'https://test-videos.co.uk/vids/jellyfish/mp4/h264/360/Jellyfish_360_10s_1MB.mp4',
         author: '@şakacı_çocuk',
         description: 'Arkadaşıma yaptığım efsane şaka hehe'
     },
     {
-        id: 'pexels-4',
-        videoUrl: 'https://videos.pexels.com/video-files/4783389/4783389-hd.mp4',
+        id: 'sample-4',
+        videoUrl: 'https://test-videos.co.uk/vids/sintel/mp4/h264/360/Sintel_360_10s_1MB.mp4',
         author: '@gamer_bro',
         description: 'Oyun oynarken ben'
     },
      {
-        id: 'pexels-5',
-        videoUrl: 'https://videos.pexels.com/video-files/6980362/6980362-hd.mp4',
+        id: 'sample-5',
+        videoUrl: 'https://test-videos.co.uk/vids/surfing/mp4/h264/360/Surfing_360_10s_1MB.mp4',
         author: '@sevimli_dostlar',
         description: 'Bu kedi tam bir komedyen!'
     }
